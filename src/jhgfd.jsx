407ddcb3873e995075999f424d61bcde96c99d4b
taaaaru <tarunverma132004@gmail.com>
2025-05-27 17:10:58 +0530

    sex sux

---
+++ src/jhgfd.jsx
@@ -0,0 +1,7 @@
+// echo "# my-project" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/taaaaru/my-project.git
+git push -u origin main
